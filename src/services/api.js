3f2e5873b4555721ebb77fe5997eb6e0abc7e872
Brunoso0 <bruno.santosbrito@outlook.com>
2025-05-26 11:57:51 -0300

    fix: update API base URL to use the correct port

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://api.jrcoffee.com.br:5000/api";
+const API_BASE_URL = "https://api.jrcoffee.com.br:5002/api";
 
 
 const api = axios.create({
