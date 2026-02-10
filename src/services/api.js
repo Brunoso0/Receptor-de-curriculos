d362fb89ed98a801c40bc67af7e97d81894800cb
Brunoso0 <bruno.santosbrito@outlook.com>
2026-02-10 15:51:48 -0300

    fix: update API_BASE_URL default to an empty string instead of localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = (process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api").replace(/\/$/, "");
+const API_BASE_URL = (process.env.REACT_APP_API_BASE_URL || "").replace(/\/$/, "");
 const API_ROOT_URL = API_BASE_URL.replace(/\/api\/?$/, "");
 
 
